a3c8a296f2f97c964275a798efd323b525875196
chvndler <chan@ady.world>
2022-09-03 20:56:41 -0400

    updates

--- src/internal/index.ts
+++ src/internal/index.ts
@@ -1,9 +1,17 @@
+export { Accordion, AccordionTrigger, AccordionItem, AccordionContent } from '../components/Accordion';
+export { AppBar } from '../components/AppBar';
+export { Badge } from '../components/Badge';
+export { Banner } from '../components/Banner';
+export { Box } from '../components/Box';
+export { Space } from '../components/Space';
+export { Button } from '../components/Button';
+export { Card } from '../components/Card';
+export { Container } from '../components/Container';
+
 // Internal components
 export * from './ColorSystem';
 export * from './BasicAtelierToast';
 export * from './DarkThemeButton';
-export * from './QuickNav';
 export * from './ThemeSwitch';
-export * from './LineDesign';
 export * from './HeroDisplay';
 export * from './TimeCode';
