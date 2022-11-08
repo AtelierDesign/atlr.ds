3e27679cb88437857808d5884389386dfd3a5662
chvndler <chan@ady.world>
2022-11-08 12:29:36 -0500

    sdsds

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,18 +6,24 @@ export const Card = styled('div', {
   font: 'inherit',
   lineHeight: '1',
   outline: 'none',
-  padding: 0,
+  padding: 32,
   textAlign: 'inherit',
   verticalAlign: 'middle',
   WebkitTapHighlightColor: 'rgba(0, 0, 0, 0)',
 
-  backgroundColor: '$panel',
+  backgroundColor: 'white',
+  // backgroundColor: '$panel',
   display: 'block',
   textDecoration: 'none',
   color: 'inherit',
   flexShrink: 0,
-  borderRadius: '$3',
+  borderRadius: 24,
   position: 'relative',
+
+  height: 'auto',
+  width: 'auto',
+
+  transition: 'all ease 0.4s',
 
   '&::before': {
     boxSizing: 'border-box',
@@ -28,25 +34,34 @@ export const Card = styled('div', {
     bottom: 0,
     left: 0,
     boxShadow: 'inset 0 0 0 1px rgba(0,0,0,.1)',
-    borderRadius: '$3',
-    pointerEvents: 'none',
+    borderRadius: 24,
+    pointerEvents: 'none'
   },
 
   variants: {
     variant: {
       interactive: {
+        transition: 'transform 200ms cubic-bezier(0.22, 1, 0.36, 1), background-color 25ms linear',
+        willChange: 'transform',
+
+        '&::before': {
+          boxShadow: '0px 10px 8px -10px rgba(22, 23, 24, 0.35), 0px 10px 20px -15px rgba(22, 23, 24, 0.2)',
+          transition: 'all 200ms cubic-bezier(0.22, 1, 0.36, 1)'
+        },
         '@hover': {
           '&:hover': {
-            '&::before': {
-              boxShadow: 'inset 0 0 0 1px rgba(0,0,0,.2)',
-            },
-          },
+            // backgroundColor: '#f2f2f2',
+            //transform: 'translateY(-1px)',
+
+            boxShadow: '0px 10px 8px -10px rgba(22, 23, 24, 0.35), 0px 10px 36px -15px rgba(22, 23, 24, 0.2)',
+            '&::before': {}
+          }
         },
         '&:focus': {
           '&::before': {
-            boxShadow: 'inset 0 0 0 1px $colors$blue8, 0 0 0 1px $colors$blue8',
-          },
-        },
+            boxShadow: 'inset 0 0 0 1px $colors$blue8, 0 0 0 1px $colors$blue8'
+          }
+        }
       },
       ghost: {
         backgroundColor: 'transparent',
@@ -55,40 +70,40 @@ export const Card = styled('div', {
         '&::before': {
           boxShadow: '0px 10px 38px -10px rgba(22, 23, 24, 0.35), 0px 10px 20px -15px rgba(22, 23, 24, 0.2)',
           opacity: '0',
-          transition: 'all 200ms cubic-bezier(0.22, 1, 0.36, 1)',
+          transition: 'all 200ms cubic-bezier(0.22, 1, 0.36, 1)'
         },
         '@hover': {
           '&:hover': {
             backgroundColor: '$panel',
             transform: 'translateY(-2px)',
             '&::before': {
-              opacity: '1',
-            },
-          },
+              opacity: '1'
+            }
+          }
         },
         '&:active': {
           transform: 'translateY(0)',
           transition: 'none',
           '&::before': {
             boxShadow: '0px 5px 16px -5px rgba(22, 23, 24, 0.35), 0px 5px 10px -7px rgba(22, 23, 24, 0.2)',
-            opacity: '1',
-          },
+            opacity: '1'
+          }
         },
         '&:focus': {
-          boxShadow: 'inset 0 0 0 1px $colors$blue8, 0 0 0 1px $colors$blue8',
-        },
+          boxShadow: 'inset 0 0 0 1px $colors$blue8, 0 0 0 1px $colors$blue8'
+        }
       },
       active: {
         transform: 'translateY(0)',
         transition: 'none',
         '&::before': {
           boxShadow: '0px 5px 16px -5px rgba(22, 23, 24, 0.35), 0px 5px 10px -7px rgba(22, 23, 24, 0.2)',
-          opacity: '1',
+          opacity: '1'
         },
         '&:focus': {
-          boxShadow: 'inset 0 0 0 1px $colors$blue8, 0 0 0 1px $colors$blue8',
-        },
-      },
-    },
-  },
+          boxShadow: 'inset 0 0 0 1px $colors$blue8, 0 0 0 1px $colors$blue8'
+        }
+      }
+    }
+  }
 });
--- src/pages/sandbox.tsx
+++ src/pages/sandbox.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Box } from '../components/Box';
 import { Canvas } from '../components/Canvas';
+import { Card } from '../components/Card';
+import { Container } from '../components/Container';
 
 function SandBox() {
   return (
     <>
       <Box css={{ backgroundColor: '$loContrast' }}>
-        <Canvas></Canvas>
+        <Canvas>
+          <Container size={'2'}>
+            <Card variant={'interactive'} css={{ minHeight: 300 }}>
+              CARD
+            </Card>
+          </Container>
+        </Canvas>
       </Box>
     </>
   );
